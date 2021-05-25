f7f744be08e60389839d8db8f09e5359be27734b
Siva <shivaabhisheka@macbook-pro.lan>
2021-05-25 14:55:48 -0400

    Proj Upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="it's a Wine Quality testing package",
+    author="sivannaa55",
+    packages=find_packages(),
+    license="MIT"
+)
+
